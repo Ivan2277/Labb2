416868e990af8fe62d37f6a62ae2ac9ddfb8e0d8
Иван Еременко <ivan.ieromenko@gmail.com>
2020-05-18 20:36:26 +0300

    Third stage

--- Labb2/Startup.cs
+++ Labb2/Startup.cs
@@ -28,6 +28,9 @@ namespace Labb2
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
